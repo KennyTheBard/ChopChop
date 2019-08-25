9d5cda1bb93d88c43a9d6393d5eca1a2f10300d3
KennyTheBard <octav_teculescu@yahoo.com>
2019-08-25 20:25:13 +0300

    Revert "Refactored the monolithic code into multiple packages"
    This reverts commit c47b7d58e3ca7f20f25b411c66345271f9c78744.

--- game.go
+++ game.go
@@ -3,26 +3,64 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
-
-	cli "./cli"
-	context "./context"
 )
+
+var playing bool
+
+func Prompt() {
+	fmt.Print(">> ")
+}
+
+func interpret(scanner *bufio.Scanner) {
+	if handler, ok := context.gCommands[getInput(scanner)]; ok {
+		handler(scanner)
+	} else {
+		fmt.Println("ERROR")
+	}
+}
+
+func nextWord(word string) string {
+	if words, ok := context.gDictionary[word]; ok {
+		return words[rand.Intn(len(words))]
+	}
+	return ""
+}
+
+func getInput(scanner *bufio.Scanner) string {
+	return strings.TrimSpace(strings.ToLower(scanner.Text()))
+}
+
+func requestInput(target string) {
+	fmt.Println("Type '" + target + "'")
+	Prompt()
+}
+
+func addItem(item string) {
+	if _, ok := context.gInventory[item]; ok {
+		context.gInventory[item]++
+	} else {
+		context.gInventory[item] = 1
+	}
+}
 
 func chopWoodHandler(scanner *bufio.Scanner) {
 	word := "wood"
-	target := gContext.GetDictionary().NextWord(word)
+	target := nextWord(word)
+	requestInput(target)
 
 	for scanner.Scan() {
-		if strings.EqualFold(target, cli.GetInput(scanner)) {
-			gContext.GetInventory().AddItem("wood")
+		if strings.EqualFold(target, getInput(scanner)) {
+			addItem("wood")
 			fmt.Println("Chopped 1 Wood!")
-		} else if strings.EqualFold("stop", cli.GetInput(scanner)) {
+		} else if strings.EqualFold("stop", getInput(scanner)) {
 			break
 		}
-		target = gContext.GetDictionary().NextWord(word)
+		target = nextWord(word)
+		requestInput(target)
 	}
 }
 
@@ -30,35 +68,54 @@ func inventoryHandler(scanner *bufio.Scanner) {
 	fmt.Print("What are you looking for in your inventory?\n>> ")
 
 	for scanner.Scan() {
-		if strings.EqualFold("stop", cli.GetInput(scanner)) {
+		if strings.EqualFold("stop", getInput(scanner)) {
 			break
 		} else {
-			if item, ok := gContext.GetInventory()[cli.GetInput(scanner)]; ok {
-				fmt.Println(cli.GetInput(scanner) + ": " + strconv.Itoa(item))
+			if item, ok := context.gInventory[getInput(scanner)]; ok {
+				fmt.Println(getInput(scanner) + ": " + strconv.Itoa(item))
 			} else {
 				fmt.Println("None")
 			}
 		}
-		cli.Prompt()
+		Prompt()
 	}
 
 }
 
-var gContext context.GameContext
+type Commands map[string]func(*bufio.Scanner)
+
+type Dictionary map[string][]string
+
+type Inventory map[string]int
+
+type GameContext struct {
+	gCommands   Commands
+	gDictionary Dictionary
+	gInventory  Inventory
+}
+
+var context GameContext
+
+func (context *GameContext) InitContext() {
+	context.gCommands = make(Commands)
+	context.gDictionary = make(Dictionary)
+	context.gInventory = make(Inventory)
+}
 
 func main() {
+	playing = true
 
-	gContext.InitContext()
-	gContext.GetCommands()["wood"] = chopWoodHandler
-	gContext.GetCommands()["inventory"] = inventoryHandler
-	gContext.GetDictionary()["wood"] = []string{"chop", "swing"}
+	context.InitContext()
+	context.gCommands["wood"] = chopWoodHandler
+	context.gCommands["inventory"] = inventoryHandler
+	context.gDictionary["wood"] = []string{"chop", "swing"}
 
 	scanner := bufio.NewScanner(os.Stdin)
 
-	cli.Prompt()
-	for scanner.Scan() {
-		gContext.GetCommands().Interpret(scanner)
-		cli.Prompt()
+	Prompt()
+	for scanner.Scan() && playing {
+		interpret(scanner)
+		Prompt()
 	}
 
 }
